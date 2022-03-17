c5493f639ffeb20b8083f5c353536aa16f4be346
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-17 09:50:21 -0300

    Home created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,15 @@
+import { Center } from  "./styled.js"
 
 export default function Home(){
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    )
-  }
-  
+    return(
+        <Center>
+            <h2>Welcome to CBank</h2>
+            
+            <labe>Register :</labe>
+            <button>  Register with Github </button>
+                
+            <labe>Login :</labe>
+            <button>   Sing In with Github </button> 
+        </Center>
+    );
+}
