1ad0cf035b2022c4f4843c830e40a55814de9bd3
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-17 09:51:20 -0300

    Home Styled

---
+++ src/pages/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Center = styled.div`
+        display:flex;
+        flex-directio: column;
+        justify-content: space-around;
+        align-items: flex-start;
+        margin-top: 3em;
+        // @media (max-width: 1200px){
+        //         
+        // }
+`
