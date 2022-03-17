9df24e2e17e756210054f104915acc19885f8345
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-17 10:22:59 -0300

    Component Header Created

---
+++ src/Header.tsx
@@ -0,0 +1,16 @@
+import {TopHeader} from "./styled.js"
+
+export default function Header() {
+    const logged = false
+
+    {!logged ? 
+        <TopHeader>
+            <h1>CBank</h1>  <a href="/login">Login</a>
+        </TopHeader> 
+      :
+        <TopHeader>
+            <h1>CBank</h1>  <a href="/login">Login</a>
+        </TopHeader>
+    }
+     
+}
