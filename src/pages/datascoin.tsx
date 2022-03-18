555b7901df0dd46b27233503d3de8d1587609c8d
Davi Serrano <daviscardoso2901@gmail.com>
2022-03-18 09:11:08 -0300

    Datas coin created

---
+++ src/pages/datascoin.tsx
@@ -0,0 +1,27 @@
+export default function DataSCoin() {
+    return(
+        <main>
+           <h2>BitCoin <strong>R$360.000,00</strong></h2>
+           <div className="chart">
+                    Grafico
+            </div>
+        
+            <div>
+                <span>Volume</span>
+                <p>1.000.000</p>
+                
+                <span>Preço</span>
+                <p>R$360.000,00</p>
+                
+                <span>última venda</span>
+                <p>U$360.000,00</p>
+                
+                <span>Variação</span>
+                <p>1.000.000</p>
+                
+                <span>Volume</span>
+                <p>1.000.000</p>
+            </div>
+        </main>
+    )
+}
